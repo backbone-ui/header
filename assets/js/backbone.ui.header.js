34ab65d35625460f8bcae69e888a84a6231dfc88
Makis Tracend <makis.tracend@gmail.com>
2017-07-04 03:23:08 -0700

    :star: view attached to (header) element

--- assets/js/backbone.ui.header.js
+++ assets/js/backbone.ui.header.js
@@ -34,15 +34,14 @@
 
 	var Header = View.extend({
 
-		el : 'body',
+		el : 'header.top',
 
 		options : {
-			headerEl : ".top",
-			mainEl : ".main",
-			detatch : false,
+			scrollEl: "body",
+			detatch: false,
 			detatchOffset : 0,
-			hide : false,
-			hideDir : "down",
+			hide: false,
+			hideDir: "down"
 			// hideOffset : 0,
 		},
 
@@ -65,8 +64,6 @@
 			}
 			this.lastScroll = scrollTop;
 
-
-
 			// if plugin option hide is true
 			if (this.options.hide) {
 
@@ -76,11 +73,11 @@
 					// check if user scroll dir is down and window is not at top
 					if ( (this.scrollDir == "down") && scrollTop > 0 ) {
 
-						$( this.options.headerEl ).addClass("ui-header-hide");
-						this.translateTop(-1*$(this.options.headerEl).height());
+						$(this.el).addClass("ui-header-hide");
+						this.translateTop(-1 * $(this.el).height());
 
 					} else {
-						$( this.options.headerEl ).removeClass("ui-header-hide");
+						$(this.el).removeClass("ui-header-hide");
 						this.translateTop();
 					}
 				}
@@ -89,26 +86,24 @@
 				if ( this.options.hideDir == "up" ) {
 
 					if ( (this.scrollDir == "up") && scrollTop > 0 ) {
-						$( this.options.headerEl ).addClass("ui-header-hide");
-						this.translateTop(-1*$(this.options.headerEl).height());
+						$(this.el).addClass("ui-header-hide");
+						this.translateTop(-1* $(this.el).height());
 
 					} else {
-						$( this.options.headerEl ).removeClass("ui-header-hide");
+						$(this.el).removeClass("ui-header-hide");
 						this.translateTop();
 					}
 				}
 			}
 
 			// if plugin option detatch is true
-			if (this.options.detatch && !$(this.options.headerEl).hasClass("ui-header-hide")) {
+			if (this.options.detatch && !$(this.el).hasClass("ui-header-hide")) {
 
 				// check if amount of user scroll is greater than the detatchOffset amount set in options
-				if (scrollTop > this.options.detatchOffset) {
-					$( this.options.headerEl ).addClass("detatch");
-				}
-
-				else {
-					$( this.options.headerEl ).removeClass("detatch");
+				if( scrollTop > this.options.detatchOffset ){
+					$(this.el).addClass("detatch");
+				} else {
+					$(this.el).removeClass("detatch");
 				}
 			}
 
@@ -128,10 +123,10 @@
 
 		translateTop: function(pixels){
 			pixels = pixels || 0;
-			$( this.options.headerEl ).css("-webkit-transform", "translate(0,"+ pixels +"px)");
-			$( this.options.headerEl ).css("-moz-transform", "translate(0,"+ pixels +"px)");
-			$( this.options.headerEl ).css("-o-transform", "translate(0,"+ pixels +"px)");
-			$( this.options.headerEl ).css("transform", "translate(0,"+ pixels +"px)");
+			$(this.el).css("-webkit-transform", "translate(0,"+ pixels +"px)");
+			$(this.el).css("-moz-transform", "translate(0,"+ pixels +"px)");
+			$(this.el).css("-o-transform", "translate(0,"+ pixels +"px)");
+			$(this.el).css("transform", "translate(0,"+ pixels +"px)");
 
 		}
 
